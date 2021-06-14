31d2e95659f5741d056732051aa63c351e69f2ce
unknown <rajranjith781@gmail.com>
2021-06-15 00:31:55 +0530

    form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
-import { StaticImage } from "gatsby-plugin-image"
 import BreadCrumb from "../components/breadcrumb"
 
 //import { Link } from "gatsby"
 const Contact = () => {
-  return(
-  <Layout>
-    <BreadCrumb name="Contact us"/>
-    <section className="container">
-      <div className="flex flex-col md:flex-row">
-        <div className="flex-1 md:px-8">
+  return (
+    <Layout>
+      <BreadCrumb name="Contact us" />
+      <section className="container">
+        <div className="text-center">
           <p>
             Let’s beat Covid together! Call our helpline for any assistance. We
             promise to do our best to ensure you get the help you need.
@@ -21,27 +19,51 @@ const Contact = () => {
             volunteer and help us grow our family
           </p>
         </div>
-        <div className="flex-1">
-          <StaticImage
-            className="z-10 covid"
-            placeholder="blurred"
-            src="../images/about/work.jpg"
-            alt="covid"
-          />
+      </section>
+      <section className="container">
+          <div className="p-3 border shadow-md md:p-5">
+            <form name="Contact Form" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="Contact Form" />
+              <div className="flex flex-col md:flex-row md:justify-between">
+                <div className="flex-1 px-1 py-5">
+                  <label className="mb-3 text-xs text-black text-opacity-50">Name*</label>
+                  <input className="block w-full py-3 border-b outline-none text-md md:w-9/12" type="text" name="name" placeholder="Name" />
+                </div>
+                <div className="flex-1 px-1 py-5">
+                  <label className="mb-3 text-xs text-black text-opacity-50">Email*</label>
+                  <input className="block w-full py-3 border-b outline-none text-md md:w-9/12" type="text" name="email" placeholder="Email" />
+                </div>
+              </div>
+              <div className="py-5">
+                <label className="mb-3 text-xs text-black text-opacity-50">Your Role: </label>
+                <select className="block w-full py-3 border-b outline-none" name="role">
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </div>
+              <div className="py-5">
+                <label className="mb-3 text-xs text-black text-opacity-50">Message*</label>
+                <textarea className="block w-full py-3 border-b outline-none" type="text" placeholder="Write your Message" name="message"></textarea>
+              </div>
+              <div>
+                <button className="px-4 py-3 text-xs font-light tracking-widest text-white uppercase rounded-full bg-u-blue" type="submit">Send message</button>
+              </div>
+
+            </form>
+          </div>
+        
+      </section>
+      <section className="container">
+        <div>
+          <p className="text-center md:text-2xl">
+            Your support saves lives! If you’re looking becoming one of our partners or collaborating with us in any other way then,
+          </p>
+
+          <a href="mailto:info@example.com"><h1 className="text-xl font-bold text-center text-gray-300 md:text-4xl">info@example.com</h1></a>
+          <a href="tel:+917777777777"><h1 className="text-xl font-bold text-center text-gray-300 md:text-4xl">+91 7777777777</h1></a>
         </div>
-      </div>
-    </section>
-    <section className="container">
-    <div>
-        <p className="text-center md:text-2xl">
-        Your support saves lives! If you’re looking becoming one of our partners or collaborating with us in any other way then,
-        </p>
-       
-        <a  href="mailto:info@example.com"><h1 className="text-xl font-bold text-center text-gray-300 md:text-4xl">info@example.com</h1></a>
-        <a  href="tel:+917777777777"><h1 className="text-xl font-bold text-center text-gray-300 md:text-4xl">+91 7777777777</h1></a>
-      </div>
-    </section>
-  </Layout>
-)
-  }
+      </section>
+    </Layout>
+  )
+}
 export default Contact
